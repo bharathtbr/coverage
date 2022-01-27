aada0839266668136145b5c01f94445f36efdf9a
Bharath Reddy T <bharath.reddy@coupa.com>
2022-01-27 12:06:22 +0530

    added

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var t = new WebApplication1.Controllers.HomeController();
+            Assert.Equal(3, t.Add(1, 2));
+        }
+    }
+}
